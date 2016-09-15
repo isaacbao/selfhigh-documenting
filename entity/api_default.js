7b4fb9d40ece67224ccef832b5fe0fef06d02e8f
isaacbao <dananad@126.com>
2016-09-15 23:40:48 +0800

    add api_default

---
+++ entity/api_default.js
@@ -0,0 +1,31 @@
+/**
+ * [默认的接口文档格式]
+ * @param {[String]} name         [接口名称]
+ * @param {[Array]} params       [接口的参数描述]
+ * @param {[String]} paramsSample [参数示例]
+ * @param {[String]} returnData   [返回的数据]
+ * @param {[String]} returnSample [返回示例]
+ * @param {[String]} comment      [备注]
+ */
+function API(name, params, paramsSample, returnData, returnSample, comment) {
+  this.name = name
+  this.params = params
+  this.returnData = returnData
+  this.returnSample = returnSample
+  this.comment = comment
+}
+
+/**
+ * [默认的参数格式]
+ * @param  {[String]} name        [参数名称]
+ * @param  {[String]} valueType   [参数值的类型]
+ * @param  {[String]} valueSample [参数示例]
+ * @param  {[String]} valueScope  [参数值取值范围]
+ * @return {[type]}             [description]
+ */
+function Param(name, valueType, valueSample, valueScope) {
+  this.name = name
+  this.valueType = valueType
+  this.valueSample = valueSample
+  this.valueScope = valueScope
+}
