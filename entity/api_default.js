88eab9cc14c73f27c837fb236981ad543a236954
unknown <dananad@126.com>
2016-09-27 20:02:40 +0800

    testing data

--- entity/api_default.js
+++ entity/api_default.js
@@ -48,12 +48,11 @@ class Update {
    * 接口的更新履历
    * @param {[Date]} updateTime    [更新时间]
    * @param {[String]} updateComment [更新内容]
-   * @param {[String]} apiName [接口名称]
+   * @param {[String]} operator [变更提交人]
    */
-  constructor(updateTime, updateComment, apiName, operator) {
+  constructor(updateTime, updateComment, operator) {
     this.updateTime = updateTime
     this.updateComment = updateComment
-    this.apiName = apiName
     this.operator = operator
   }
 }
