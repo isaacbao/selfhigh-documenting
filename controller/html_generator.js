1d41c36940fc01493577971a360dbbe04c0d274c
isaacbao <dananad@126.com>
2016-10-04 21:19:39 +0800

    ..

--- controller/html_generator.js
+++ controller/html_generator.js
@@ -10,4 +10,8 @@ exports.generate(document) {
 
 function getDefaultHead() {
   let $ = cheerio.load('<head></head>')
+  console.log($(head))
+  $(head).append('<title id="apiDocTitle">后端接口</title>')
 }
+
+getDefaultHead()
